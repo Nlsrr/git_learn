a247db50ea44376604557d86d680948eb1a367bd
niu <1014134458@qq.com>
2021-11-21 18:24:10 +0800

    add a outline 123

--- src/com/niuge/www/helloGit.java
+++ src/com/niuge/www/helloGit.java
@@ -4,5 +4,6 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("helo git!");
         System.out.println("hi~~");
+        System.out.println("123");
     }
 }
